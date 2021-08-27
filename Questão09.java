1b87a8c583d202001efe452416fb338dcc16c7fe
Magnum2222 <magnumsapf@gmail.com>
2021-08-26 22:22:56 -0300

    Create Questão09.java

---
+++ Questão09.java
@@ -0,0 +1,19 @@
+public class Questão09 {
+    public static void main(String[] args) { 
+        for (int i = 2; i <= 8; i = i + 2) { 
+            for (int j = i; j <= 4; j++) { 
+                for (int k = 1; k <= j; k = k + i) { 
+                    System.out.println(i + ", " + j + ", " + k); 
+                } 
+            } 
+        } 
+    }
+}
+
+/* As sáidas serão : 
+2, 2, 1
+2, 3, 1
+2, 3, 3
+2, 4, 1
+2, 4, 3
+4, 4, 1 */
