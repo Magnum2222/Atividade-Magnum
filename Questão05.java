3d48801acc6dc4d91a27ef23fcefc128aefb3b0a
Magnum2222 <magnumsapf@gmail.com>
2021-08-26 22:22:45 -0300

    Create Questão05.java

---
+++ Questão05.java
@@ -0,0 +1,12 @@
+public class Questão05{
+    public static void main(String[]args){
+        for(int Calculador = 1; Calculador <=10; Calculador ++){
+            System.out.println("Tabela de Numeros "+Calculador);
+        for(int Calculador1 = 1; Calculador1 <= 10; Calculador++){
+            System.out.println(Calculador+"X"+Calculador2+"="+(Calculador1*Calculador));
+
+        }
+        }
+
+    }
+}
